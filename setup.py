4d1a4e1596728c5c71eb39f97a7fe3f64becfd84
Michael Seifert <michaelseifert04@yahoo.de>
2016-09-13 15:40:14 +0200

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='iteration_utilities',
-      version='0.0.2',
+      version='0.0.3dev',
 
       description='some utilities for iterations.',
       long_description=('Itertools recipes from the python package docs and '
