3138dce3108d7e27b3da9a709f4ea5185abe88af
Michael Seifert <michaelseifert04@yahoo.de>
2016-09-24 04:17:41 +0200

    add docstrings

--- src_c/simplecallbacks.c
+++ src_c/simplecallbacks.c
@@ -12,15 +12,133 @@ simplecallbacks_returnTrue(PyObject *self, PyObject *args, PyObject *keywds) {
     Py_RETURN_TRUE;
 }
 
+PyDoc_STRVAR(simplecallbacks_returnTrue_doc,
+"return_True(*args, **kwargs) -> True\n\
+\n\
+Always return ``True``.\n\
+\n\
+Parameters\n\
+----------\n\
+args, kwargs \n\
+    any number of positional or keyword parameter.\n\
+\n\
+Returns\n\
+-------\n\
+true : bool\n\
+    Always returns ``True``.\n\
+\n\
+Examples\n\
+--------\n\
+This function is equivalent to ``lambda *args, **kwargs: True`` but significantly\n\
+faster::\n\
+\n\
+    >>> from iteration_utilities import return_True\n\
+    >>> return_True(1, 2, 3, 4, a=100)\n\
+    True\n\
+");
+
 static PyObject*
 simplecallbacks_returnFalse(PyObject *self, PyObject *args, PyObject *keywds) {
     Py_RETURN_FALSE;
 }
 
+PyDoc_STRVAR(simplecallbacks_returnFalse_doc,
+"return_False(*args, **kwargs) -> False\n\
+\n\
+Always return ``False``.\n\
+\n\
+Parameters\n\
+----------\n\
+args, kwargs \n\
+    any number of positional or keyword parameter.\n\
+\n\
+Returns\n\
+-------\n\
+false : bool\n\
+    Always returns ``False``.\n\
+\n\
+Examples\n\
+--------\n\
+This function is equivalent to ``lambda *args, **kwargs: False`` but significantly\n\
+faster::\n\
+\n\
+    >>> from iteration_utilities import return_False\n\
+    >>> return_False(1, 2, 3, 4, a=100)\n\
+    False\n\
+");
+
 static PyObject*
 simplecallbacks_returnNone(PyObject *self, PyObject *args, PyObject *keywds) {
     Py_RETURN_NONE;
 }
+
+PyDoc_STRVAR(simplecallbacks_returnNone_doc,
+"return_None(*args, **kwargs) -> None\n\
+\n\
+Always return ``None``.\n\
+\n\
+Parameters\n\
+----------\n\
+args, kwargs \n\
+    any number of positional or keyword parameter.\n\
+\n\
+Returns\n\
+-------\n\
+none : None\n\
+    Always returns ``None``.\n\
+\n\
+Examples\n\
+--------\n\
+This function is equivalent to ``lambda *args, **kwargs: None`` but significantly\n\
+faster::\n\
+\n\
+    >>> from iteration_utilities import return_None\n\
+    >>> return_None(1, 2, 3, 4, a=100) is None\n\
+    True\n\
+");
+
+static PyObject*
+simplecallbacks_returnFirstPositionalArgument(PyObject *self, PyObject *args, PyObject *keywds) {
+    PyObject *first;
+    Py_ssize_t tuple_size = PyTuple_Size(args);
+
+    if (tuple_size == 0) {
+        PyErr_Format(PyExc_TypeError, "Expected at least one positional argument.");
+        return NULL;
+    }
+
+    first = PyTuple_GetItem(args, (Py_ssize_t)0);
+    if (first == NULL)
+        return NULL;
+
+    Py_INCREF(first);
+    return first;
+}
+
+PyDoc_STRVAR(simplecallbacks_returnFirstPositionalArgument_doc,
+"return_first_positional_argument(*args, **kwargs) -> args[0]\n\
+\n\
+Always return the first positional argument given to the function.\n\
+\n\
+Parameters\n\
+----------\n\
+args, kwargs \n\
+    any number of positional or keyword parameter.\n\
+\n\
+Returns\n\
+-------\n\
+first_positional_argument : any type\n\
+    Always returns the first positional argument given to the function.\n\
+\n\
+Examples\n\
+--------\n\
+This function is equivalent to ``lambda *args, **kwargs: args[0]`` but significantly\n\
+faster::\n\
+\n\
+    >>> from iteration_utilities import return_first_positional_argument\n\
+    >>> return_first_positional_argument(1, 2, 3, 4, a=100)\n\
+    1\n\
+");
 
 /* Functions comparing to a specific value.
  *
@@ -35,13 +153,52 @@ simplecallbacks_IsNone(PyObject *self, PyObject *args) {
     Py_RETURN_FALSE;
 }
 
+PyDoc_STRVAR(simplecallbacks_IsNone_doc,
+"is_None(value) -> bool\n\
+\n\
+Returns ``True`` if `value` is ``None``, otherwise ``False``.\n\
+\n\
+Parameters\n\
+----------\n\
+value : any type \n\
+    The value to test for ``None``.\n\
+\n\
+Returns\n\
+-------\n\
+is_none : bool\n\
+    ``True`` if `value` is ``None`` otherwise it returns ``False``.\n\
+\n\
+Examples\n\
+--------\n\
+This function is equivalent to ``lambda x: x is None`` but significantly\n\
+faster::\n\
+\n\
+    >>> from iteration_utilities import is_None\n\
+    >>> is_None(None)\n\
+    True\n\
+    >>> is_None(False)\n\
+    False\n\
+\n\
+This can be used for example to remove all ``None`` from an iterable::\n\
+\n\
+    >>> import sys\n\
+    >>> import itertools\n\
+    >>> filterfalse = itertools.ifilterfalse if sys.version_info.major == 2 else itertools.filterfalse\n\
+    >>> list(filterfalse(is_None, [1, None, 3, 4, 5, None, 7]))\n\
+    [1, 3, 4, 5, 7]\n\
+");
+
 /* Wrapper functions for simple arithmetic functions.
  *
  * square   : identical to pow(x, 2)
  */
 
+// global variable
 static PyObject *simplecallback_long_2 = NULL;
 
+// return global variable or if it's NULL initialize it.
+// TODO: That's really weird to do it like this, no idea what the alternative
+//       would be...
 static PyObject*
 simplecallback_get_2(void) {
     if (simplecallback_long_2 == NULL)
@@ -55,6 +212,33 @@ simplecallbacks_square(PyObject *self, PyObject *args) {
     return PyNumber_Power(args, simplecallback_get_2(), Py_None);
 }
 
+PyDoc_STRVAR(simplecallbacks_square_doc,
+"square(value) -> squared_value\n\
+\n\
+Returns the squared `value`.\n\
+\n\
+Parameters\n\
+----------\n\
+value : any type\n\
+    The value to be squared. The type of the `value` must support ``pow``.\n\
+\n\
+Returns\n\
+-------\n\
+square : any type\n\
+    Returns ``value**2``.\n\
+\n\
+Examples\n\
+--------\n\
+It is not possible to apply ``functools.partial`` to ``pow`` so that one has\n\
+a one-argument square function and is significantly faster than ``lambda x: x**2``::\n\
+\n\
+    >>> from iteration_utilities import square\n\
+    >>> square(1)\n\
+    1\n\
+    >>> square(2.0)\n\
+    4.0\n\
+");
+
 
 //Method definition object for this extension, these argumens mean:
 //ml_name: The name of the method
@@ -65,21 +249,36 @@ simplecallbacks_square(PyObject *self, PyObject *args) {
 //ml_doc:  Contents of this method's docstring
 static PyMethodDef
 simplecallback_methods[] = {
-    {"returnTrue", (PyCFunction)simplecallbacks_returnTrue, METH_VARARGS | METH_KEYWORDS,
-     "returnTrue(*args, **kwargs) -- Return ``True``."
-    },
-    {"returnFalse", (PyCFunction)simplecallbacks_returnFalse, METH_VARARGS | METH_KEYWORDS,
-     "returnFalse(*args, **kwargs) -- Return ``False``."
-    },
-    {"returnNone", (PyCFunction)simplecallbacks_returnNone, METH_VARARGS | METH_KEYWORDS,
-     "returnNone(*args, **kwargs) -- Return ``None``."
-    },
-    {"square", (PyCFunction)simplecallbacks_square, METH_O,
-     "square(value) -- Return the value squared."
-    },
-    {"isNone", (PyCFunction)simplecallbacks_IsNone, METH_O,
-     "isNone(value) -- Return if the value is ``None``."
-    },
+    {"return_True",
+     (PyCFunction)simplecallbacks_returnTrue,
+     METH_VARARGS | METH_KEYWORDS,
+     simplecallbacks_returnTrue_doc},
+
+    {"return_False",
+     (PyCFunction)simplecallbacks_returnFalse,
+     METH_VARARGS | METH_KEYWORDS,
+     simplecallbacks_returnFalse_doc},
+
+    {"return_None",
+     (PyCFunction)simplecallbacks_returnNone,
+     METH_VARARGS | METH_KEYWORDS,
+     simplecallbacks_returnNone_doc},
+
+    {"return_first_positional_argument",
+     (PyCFunction)simplecallbacks_returnFirstPositionalArgument,
+     METH_VARARGS | METH_KEYWORDS,
+     simplecallbacks_returnFirstPositionalArgument_doc},
+
+    {"square",
+     (PyCFunction)simplecallbacks_square,
+     METH_O,
+     simplecallbacks_square_doc},
+
+    {"is_None",
+     (PyCFunction)simplecallbacks_IsNone,
+     METH_O,
+     simplecallbacks_IsNone_doc},
+
     {NULL, NULL}
 };
 
@@ -90,14 +289,12 @@ simplecallback_methods[] = {
   static struct PyModuleDef
   simplecallback_definition = {
     PyModuleDef_HEAD_INIT,
-    "simplecallbacks",
-    "\nSimple Callbacks\n^^^^^^^^^^^^^^^^^^\n",
-    -1,
-    simplecallback_methods,
-    NULL,
-    NULL,
-    NULL,
-    NULL
+    "simplecallbacks",                          /* module name */
+    "\nSimple Callbacks\n^^^^^^^^^^^^^^^^^^\n", /* module docstring */
+    -1,                                         /* no idea */
+    simplecallback_methods,                     /* module methods */
+
+    NULL, NULL, NULL, NULL
   };
 
   //Module initialization
@@ -111,8 +308,11 @@ simplecallback_methods[] = {
     int i;
     PyObject *m;
     char *name;
+
+    // Fill in classes! Must be synced with the Python2 version of module init
+    // a few lines later.
     PyTypeObject *typelist[] = {
-        //&filterfalse2_type,
+        //&cls_type,
         NULL
     };
 
@@ -120,6 +320,8 @@ simplecallback_methods[] = {
     if (m == NULL)
         return NULL;
 
+    // Add classes to the module but only use the name starting after the first
+    // occurence of ".".
     for (i=0 ; typelist[i] != NULL ; i++) {
         if (PyType_Ready(typelist[i]) < 0)
             return NULL;
@@ -131,7 +333,9 @@ simplecallback_methods[] = {
 
     return m;
   }
+
 #else
+
   void
   initsimplecallbacks(void)
   {
@@ -139,14 +343,21 @@ simplecallback_methods[] = {
     int i;
     PyObject *m;
     char *name;
+
+    // Fill in classes! Must be synced with the Python3 version of module init
+    // a few lines earlier.
     PyTypeObject *typelist[] = {
-        //&filterfalse_type,
+        //&cls_type,
         NULL
     };
-    m = Py_InitModule("simplecallbacks", simplecallback_methods);
+
+    m = Py_InitModule("simplecallbacks",
+                      simplecallback_methods);
     if (m == NULL)
         return;
 
+    // Add classes to the module but only use the name starting after the first
+    // occurence of ".".
     for (i=0 ; typelist[i] != NULL ; i++) {
         if (PyType_Ready(typelist[i]) < 0)
             return;
