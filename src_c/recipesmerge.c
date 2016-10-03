e3c8d82af8d8ed96869779703cb63f59bc1cb93a
Michael Seifert <michaelseifert04@yahoo.de>
2016-10-03 06:01:16 +0200

    fixed

--- src_c/recipesmerge.c
+++ src_c/recipesmerge.c
@@ -24,15 +24,16 @@ helper_bisect_right(PyObject *list, PyObject *item, Py_ssize_t hi, int cmpop)
     // It is likely that the next item of the yielded iterable is also the
     // next item to be yielded (?!) so check if it's fit to be included at the
     // end.
-    litem = PyTuple_GET_ITEM(list, hi);
+    litem = PyTuple_GET_ITEM(list, hi-1);
     if (litem == NULL) {
         return -1;
     }
     res = PyObject_RichCompareBool(item, litem, cmpop);
     if (res) {
-        return hi + 1;
+        return hi;
     }
 
+    hi--;
 
     while (lo < hi) {
         /* The (size_t)cast ensures that the addition and subsequent division
