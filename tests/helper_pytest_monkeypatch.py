d875f311e52961d11e76ab2a1191e7843178ce8e
Michael Seifert <michaelseifert04@yahoo.de>
2016-10-19 01:02:18 +0200

    undo monkeypatched pytest.raises because the issue was resolved by adding gc.collect() to the memory-leak test function

--- tests/helper_pytest_monkeypatch.py
+++ tests/helper_pytest_monkeypatch.py
@@ -1,18 +1,6 @@
 # 3rd party
 import pytest
-from contextlib import contextmanager
 
-# Memory leak with version 3.0.2 which makes it damn hard to test for memory
-# leaks on CI. Remove this if noone uses 3.0.2 anymore!
-# https://github.com/pytest-dev/pytest/issues/1965
-if pytest.__version__ not in ('3.0.2', '3.0.3'):
-    pytest_raises = pytest.raises
-else:
-    @contextmanager
-    def pytest_raises(exc):
-        try:
-            yield
-        except exc:
-            pass
-        else:
-            raise TypeError('did not raise {0!s}'.format(exc))
+# Monkeypatch is not needed because the issue was just some cyclic references
+# that are resolved by adding "gc.collect()" in the memoryleak helper.
+pytest_raises = pytest.raises
