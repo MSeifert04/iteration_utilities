c0f3da3c0d298dbf8f965ce3b7720bdae53935af
Michael Seifert <michaelseifert04@yahoo.de>
2016-10-21 20:56:05 +0200

    edited some meta options for setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(name='iteration_utilities',
       keywords=['functional', 'functools', 'generator',
                 'itertools', 'iteration', 'iterator',
                 'operators', 'performance', 'reduce', 'utility'],
+      platforms=["Windows", "Linux", "Unix"],
 
       classifiers=[
         'Development Status :: 4 - Beta',
