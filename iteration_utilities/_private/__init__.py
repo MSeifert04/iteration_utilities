0cfda2fbffe11c563f91884113bf6c54100f33ae
Michael Seifert <michaelseifert04@yahoo.de>
2017-03-14 20:08:31 +0100

    changed some of the style of Iterable-evaluations

---
+++ iteration_utilities/_private/__init__.py
@@ -0,0 +1,10 @@
+"""
+from iteration_utilities._private import create_functions_table, create_method_lists
+from iteration_utilities.core import _Base, Iterable, InfiniteIterable, ManyIterables
+
+print(create_functions_table.create_included_function_list_readme())
+print(create_functions_table.create_included_function_list())
+print(create_method_lists._get_methods(Iterable, 2))
+print(create_method_lists._get_methods(InfiniteIterable, 2))
+print(create_method_lists._get_methods(ManyIterables, 2))
+"""
