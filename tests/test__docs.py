2812d0ec2594cdedfacf3f36ff8c1b3f3e71655c
MSeifert04 <michaelseifert04@yahoo.de>
2019-11-01 20:27:08 +0100

    Add new statistics functions as methods on Iterable.

--- tests/test__docs.py
+++ tests/test__docs.py
@@ -6,6 +6,19 @@ import pytest
 
 import iteration_utilities
 from iteration_utilities import _iteration_utilities
+from iteration_utilities._utils import GE_PY36, GE_PY38
+
+
+# Some of the methods on Iterable don't work under specific conditions.
+# To prevent doctest failures these methods are removed during test-collection.
+# This isn't very nice but it should work.
+_delete_methods = []
+if not GE_PY38:
+    _delete_methods += ['get_fmean', 'get_quantiles', 'get_multimode', 'get_geometric_mean']
+if not GE_PY36:
+    _delete_methods += ['get_harmonic_mean']
+for _methodname in _delete_methods:
+    delattr(iteration_utilities.Iterable, _methodname)
 
 
 @pytest.mark.parametrize("mod",
