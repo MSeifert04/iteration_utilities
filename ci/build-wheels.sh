72c58a2886a4482353175220a1b85a4b0e5f73ce
Michael Seifert <michaelseifert04@yahoo.de>
2022-03-16 00:11:48 +0100

    update

--- ci/build-wheels.sh
+++ ci/build-wheels.sh
@@ -2,10 +2,6 @@
 set -e -x
 
 # Compile wheels
-/opt/python/cp35-cp35m/bin/pip install pip
-/opt/python/cp35-cp35m/bin/pip install setuptools wheel
-/opt/python/cp35-cp35m/bin/pip wheel /io/ -w wheelhouse/
-
 /opt/python/cp36-cp36m/bin/pip install pip
 /opt/python/cp36-cp36m/bin/pip install setuptools wheel
 /opt/python/cp36-cp36m/bin/pip wheel /io/ -w wheelhouse/
@@ -21,6 +17,10 @@ set -e -x
 /opt/python/cp39-cp39/bin/pip install pip
 /opt/python/cp39-cp39/bin/pip install setuptools wheel
 /opt/python/cp39-cp39/bin/pip wheel /io/ -w wheelhouse/
+
+/opt/python/cp310-cp310m/bin/pip install pip
+/opt/python/cp310-cp310m/bin/pip install setuptools wheel
+/opt/python/cp310-cp310m/bin/pip wheel /io/ -w wheelhouse/
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
